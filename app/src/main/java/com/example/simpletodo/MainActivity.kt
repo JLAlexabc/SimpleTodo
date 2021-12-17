7e4b65eb01f59eb8e5455661bbfd2df8414de88d
AlexJ <linctjabc@gmail.com>
2021-12-17 10:39:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.simpletodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.FileUtils
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileSystem
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+
+    var listOfTasks = mutableListOf<String>()
+    lateinit var adapter:TaskItemAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
+        val onLongClickListener = object : TaskItemAdapter.OnLongClickListener {
+            override fun onItemLongClicked(position: Int) {
+                //1. remove the item from list
+                listOfTasks.removeAt(position)
+                saveItems()//save into file
+                //2. update the adapter the data changed
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        loadItems() //read data from file and save into listOfTasks
+
+        val recyclerView =findViewById<RecyclerView>(R.id.recyclerView)
+        adapter = TaskItemAdapter(listOfTasks,onLongClickListener)
+        //conect with adapter
+        recyclerView.adapter=adapter
+        recyclerView.layoutManager=LinearLayoutManager(this)
+
+        //user click the button
+        findViewById<Button>(R.id.button).setOnClickListener{
+            //1. get the text user input @id/addTaskField
+            val userInputTask = inputTextField.text.toString()
+
+            //2. add the input string to the tasks listOfTask
+            listOfTasks.add(userInputTask)
+            saveItems()//save into file
+
+            //notify the adapter that data be updated
+            adapter.notifyItemInserted(listOfTasks.size-1)
+
+            //3. clear out the input field
+            inputTextField.setText("")
+
+
+        }
+    }
+    //1. save data into file
+    fun saveItems(){
+        try {
+            org.apache.commons.io.FileUtils.writeLines(getDataFile(),listOfTasks)
+        }catch (ioException:IOException){
+            ioException.printStackTrace()
+        }
+
+    }
+    //2.get file object
+    fun getDataFile(): File {
+        return File(filesDir,"data.txt")
+    }
+    //3. read the data from file
+    fun loadItems(){
+        try {
+            listOfTasks=org.apache.commons.io.FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        }catch (ioException:IOException){
+            ioException.printStackTrace()
+        }
+    }
+}
